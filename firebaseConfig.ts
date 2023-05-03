ece8e16d97be994d96abb55015037e4ec171f2a4
Omar JARRAY <ojarray@iga-tunisie.com>
2023-05-03 14:12:00 +0100

    init Firebase

---
+++ firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAaLZJH868ziE29YWB4AV42v-YSHatRcrk',
+    authDomain: 'sadka-31ab2.firebaseapp.com',
+    databaseURL: 'https://project-id.firebaseio.com',
+    projectId: 'sadka-31ab2',
+    storageBucket: 'sadka-31ab2.appspot.com',
+    messagingSenderId: '291779193080',
+    appId: '1:291779193080:web:774ba23c7daeb7ae28730d'/*,
+    measurementId: 'G-measurement-id',*/
+};
+
+initializeApp(firebaseConfig);
